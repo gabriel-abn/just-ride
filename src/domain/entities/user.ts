3e346a3e89a35aca998d1feac733f7f671b6a7ac
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-03-16 19:46:40 -0300

    feat(User): :sparkles: created user domain class
    User class has no domain business rules yet.

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,38 @@
+import { Entity } from "@/domain/common";
+import crypto from "crypto";
+
+export type UserProps = {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  emailVerified: boolean;
+  phone: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export class User extends Entity<UserProps> {
+  private constructor(id: string, props: UserProps) {
+    super(id, props);
+  }
+
+  static create(params: {
+    username: string;
+    email: string;
+    password: string;
+  }): User {
+    const id = crypto.randomUUID().split("-").join("").toUpperCase();
+
+    return new User(id, {
+      username: params.username,
+      name: "",
+      email: params.email,
+      password: params.password,
+      emailVerified: false,
+      phone: "",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+}
