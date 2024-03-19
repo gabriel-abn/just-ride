02d0e50f0e39cdc1c848b90078b2aeed798ac423
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-03-18 22:12:11 -0300

    feat: :sparkles: added props attribute getter

--- src/domain/common/entity.ts
+++ src/domain/common/entity.ts
@@ -1,13 +1,17 @@
 export abstract class Entity<T> {
   protected readonly _id: string;
-  protected readonly props: T;
+  protected readonly _props: T;
 
   constructor(id: string, props: T) {
     this._id = id;
-    this.props = props;
+    this._props = props;
   }
 
   get id(): string {
     return this._id;
+  }
+
+  get props(): T {
+    return this.props;
   }
 }
