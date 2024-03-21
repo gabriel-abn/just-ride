8c4e0ef66f4200ce7e68499acbd542a27a22a59b
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-03-21 11:10:55 -0300

    feat(User): :sparkles: implemented drizzle orm to repository

--- src/infra/persistence/repositories/user-repository.ts
+++ src/infra/persistence/repositories/user-repository.ts
@@ -1,31 +1,89 @@
 import { ApplicationError } from "@/application/common";
 import type { IUserRepository } from "@/application/protocols/repositories";
-import type { User } from "@/domain/entities";
+import { User } from "@/domain/entities";
+import { eq } from "drizzle-orm";
+import postgres from "../database/relational/postgres";
+import { userTable } from "../database/relational/schema";
 
 class UserRepository implements IUserRepository {
-  users: User[];
+  constructor(private readonly db = postgres) {}
 
-  constructor() {
-    this.users = [];
+  async get(
+    filter?: Partial<{ id: string; email: string }> | undefined
+  ): Promise<User> {
+    let user: User;
+
+    try {
+      if (filter) {
+        const { id, email } = filter;
+
+        if (id) {
+          const [result] = await this.db
+            .select()
+            .from(userTable)
+            .where(eq(userTable.id, id));
+
+          user = User.restore(result.id, {
+            ...result,
+            emailVerified: result.verified,
+            createdAt: new Date(result.created_at),
+            updatedAt: new Date(result.updated_at),
+          });
+
+          return user;
+        }
+
+        if (email) {
+          const [result] = await this.db
+            .select()
+            .from(userTable)
+            .where(eq(userTable.email, email));
+
+          user = User.restore(result.id, {
+            ...result,
+            emailVerified: result.verified,
+            createdAt: new Date(result.created_at),
+            updatedAt: new Date(result.updated_at),
+          });
+
+          return user;
+        }
+
+        throw new Error("UserRepository.get: Invalid filter.");
+      }
+
+      throw new Error("UserRepository.get: No filter provided.");
+    } catch (error: any) {
+      throw new Error(error);
+    }
   }
 
   async save(user: User): Promise<void> {
     try {
-      if (this.users.length >= 1) {
-        this.users.find((u) => {
-          if (u.props.email == user.props.email)
-            throw new ApplicationError(
-              "Email already in use.",
-              "EMAIL_ALREADY_IN_USE"
-            );
-        });
+      const exists = await this.get({ email: user.props.email });
+
+      if (exists) {
+        throw new ApplicationError(
+          "User already exists.",
+          "EMAIL_ALREADY_IN_USE"
+        );
       }
 
-      this.users.push(user);
+      await this.db.insert(userTable).values({
+        id: user.id,
+        name: user.props.name,
+        email: user.props.email,
+        username: user.props.username,
+        password: user.props.password,
+        verified: user.props.emailVerified,
+        phone: user.props.phone,
+        created_at: user.props.createdAt.toISOString(),
+        updated_at: user.props.updatedAt.toISOString(),
+      });
     } catch (error: any) {
       throw new Error(error);
     }
   }
 }
 
-export default new UserRepository();
+export default UserRepository;
