1917321cc5b6a4b664c2609e37e3a1c473c74cc3
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-03-21 11:08:10 -0300

    feat(User): :art: added get method to repository interface

--- src/application/protocols/repositories/user-repository.ts
+++ src/application/protocols/repositories/user-repository.ts
@@ -2,4 +2,5 @@ import type { User } from "@/domain/entities";
 
 export interface IUserRepository {
   save(user: User): Promise<void>;
+  get(filter?: Partial<{ id: string; email: string }>): Promise<User>;
 }
