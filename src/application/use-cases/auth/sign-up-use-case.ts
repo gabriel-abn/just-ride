67eb5d91b9d38eab4b66386ff6baa5e14b39bd2f
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-03-24 10:39:00 -0300

    feat(SignUp): :ambulance: added user verification to use case

--- src/application/use-cases/auth/sign-up-use-case.ts
+++ src/application/use-cases/auth/sign-up-use-case.ts
@@ -1,3 +1,4 @@
+import { ApplicationError } from "@/application/common";
 import type { IUserRepository } from "@/application/protocols/repositories";
 import type { IHasher } from "@/application/protocols/security";
 import { User } from "@/domain/entities";
@@ -10,6 +11,15 @@ export class SignUpUseCase implements SignUp {
   ) {}
 
   async execute(params: SignUp.Params): Promise<SignUp.Result> {
+    const userExists = await this.userRepo.get({ email: params.email });
+
+    if (userExists) {
+      throw new ApplicationError(
+        "User already exists.",
+        "EMAIL_ALREADY_IN_USE"
+      );
+    }
+
     const hashedPassword = await this.hasher.hash(params.password);
 
     const user = User.create({
