8ee86ff56a96c819f14b58875e97e1c2ecc1c3ce
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-03-24 22:07:52 -0300

    test(Login): :test_tube: added tests

--- tests/cases/sign-up-and-login.test.ts
+++ tests/cases/sign-up-and-login.test.ts
@@ -14,7 +14,7 @@ describe("Create an account/Sign up", () => {
     };
   });
 
-  it("should receive email and password", async () => {
+  it("should create with email and password", async () => {
     const response = await axios({
       method: "post",
       url: `http://localhost:${env.PORT}/auth/sign-up`,
@@ -40,8 +40,6 @@ describe("Create an account/Sign up", () => {
       validateStatus: () => true,
     });
 
-    console.log(response.data);
-
     expect(response.data.response).toHaveProperty(
       "error",
       "EMAIL_ALREADY_IN_USE"
@@ -61,5 +59,59 @@ describe("Create an account/Sign up", () => {
       "error",
       "INVALID_MISSING_PARAMS"
     );
+    expect(response.status).toEqual(400);
+  });
+});
+
+describe("Login", () => {
+  let user: any;
+
+  beforeAll(async () => {
+    user = {
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: "Gabriel!2343",
+    };
+
+    await axios({
+      method: "post",
+      url: `http://localhost:${env.PORT}/auth/sign-up`,
+      data: { ...user, password: "gabriel123" },
+      validateStatus: () => true,
+    });
+  });
+
+  it("should receive 400 if email not found", async () => {
+    const response = await axios({
+      method: "post",
+      url: `http://localhost:${env.PORT}/auth/login`,
+      data: { email: faker.internet.email(), password: user.password },
+    });
+
+    expect(response.data.response).toHaveProperty("error", "EMAIL_NOT_FOUND");
+    expect(response.status).toEqual(400);
+  });
+
+  it("should receive 400 if password is invalid", async () => {
+    const response = await axios({
+      method: "post",
+      url: `http://localhost:${env.PORT}/auth/login`,
+      data: { email: user.email, password: "gabriel123" },
+      validateStatus: () => true,
+    });
+
+    expect(response.data.response).toHaveProperty("error", "INVALID_PASSWORD");
+    expect(response.status).toEqual(400);
+  });
+
+  it("should login with email and password", async () => {
+    const response = await axios({
+      method: "post",
+      url: `http://localhost:${env.PORT}/auth/login`,
+      data: { email: user.email, password: user.password },
+      validateStatus: () => true,
+    });
+
+    expect(response.data.response).toHaveProperty("token");
   });
 });
